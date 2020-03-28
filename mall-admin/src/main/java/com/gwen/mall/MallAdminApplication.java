d633f3291a2c2c1d4c10f7dca8dc60d6c4dbf85e
Scorpion <1013397466@qq.com>
2020-03-29 01:02:10 +0800

    mall

--- mall-admin/src/main/java/com/gwen/mall/MallAdminApplication.java
+++ mall-admin/src/main/java/com/gwen/mall/MallAdminApplication.java
@@ -8,6 +8,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan({"com.gwen.*.dao"})
+/**
+ * spring cloud中discovery service有许多种实现（eureka、consul、zookeeper等等）
+ * .@EnableDiscoveryClient基于spring-cloud-commons,
+ * .@EnableEurekaClient基于spring-cloud-netflix。
+ * 其实用更简单的话来说，就是如果选用的注册中心是eureka，那么就推荐@EnableEurekaClient,
+ * 如果是其他的注册中心，那么推荐使用@EnableDiscoveryClient。
+ */
 @EnableEurekaClient
 public class MallAdminApplication {
 
