adf87ad3f0e85a7394b710564ea1a55cbb453a85
Scorpion <1013397466@qq.com>
2020-03-29 00:55:40 +0800

    mall

---
+++ mall-gateway-zuul/src/main/java/com/gwen/mall/GatewayZuulApplication.java
@@ -0,0 +1,28 @@
+package com.gwen.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Description：
+ * Created with IntelliJ IDEA
+ * Created By guowen
+ * Date: 2020/3/28
+ * Time: 23:00
+ */
+
+/**@SpringCloudApplication
+ * 其中@EnableZuulProxy是@EnableZuulServer的加强，
+ * .@SpringCloudApplication会包含@EnableEurekaClient，所以其实@EnableEurekaClient不需要写
+ */
+@SpringBootApplication
+@EnableZuulProxy  //支持网关路由
+@EnableDiscoveryClient
+public class GatewayZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayZuulApplication.class, args);
+    }
+}
